862188ad77418daef50ef104e2f5c4ea9cdd5aa7
wudonglin <296734078@qq.com>
2018-08-31 13:41:13 +0800

    编写个人中心页面

--- app/src/main/java/com/auto/mobile/moudle/home/view/person/PersonFragment.java
+++ app/src/main/java/com/auto/mobile/moudle/home/view/person/PersonFragment.java
@@ -1,13 +1,18 @@
 package com.auto.mobile.moudle.home.view.person;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.auto.mobile.R;
+import com.auto.mobile.common.application.MyApplication;
 import com.auto.mobile.common.base.BaseFragment;
 import com.auto.mobile.common.view.RoundImageView;
+import com.auto.mobile.moudle.system.GetServiceActivity;
+import com.auto.mobile.moudle.system.ReleaseActivity;
+import com.auto.mobile.moudle.system.SettingActivity;
 import com.qmuiteam.qmui.widget.QMUITopBar;
 
 import butterknife.BindView;
@@ -55,14 +60,21 @@ public class PersonFragment extends BaseFragment {
     //点击事件
     @OnClick({R.id.img_head, R.id.tv_release, R.id.tv_service, R.id.tv_setup})
     public void onViewClicked(View view) {
+        Intent intent;
         switch (view.getId()) {
             case R.id.img_head:
                 break;
             case R.id.tv_release:
+                intent=new Intent(getActivity(), ReleaseActivity.class);
+                startActivity(intent);
                 break;
             case R.id.tv_service:
+                intent=new Intent(getActivity(), GetServiceActivity.class);
+                startActivity(intent);
                 break;
             case R.id.tv_setup:
+                intent=new Intent(getActivity(), SettingActivity.class);
+                startActivity(intent);
                 break;
         }
     }
--- app/src/main/java/com/auto/mobile/moudle/system/GetServiceActivity.java
+++ app/src/main/java/com/auto/mobile/moudle/system/GetServiceActivity.java
@@ -1,22 +1,48 @@
 package com.auto.mobile.moudle.system;
 
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.auto.mobile.R;
 import com.auto.mobile.common.base.BaseActivity;
+import com.qmuiteam.qmui.widget.QMUITopBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 获得的服务
  */
 public class GetServiceActivity extends BaseActivity {
 
+    @BindView(R.id.topBar)
+    QMUITopBar topBar;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
     @Override
     protected int getContextViewId() {
         return 0;
     }
 
-    @Override
-    public void setContentView(View view) {
-        super.setContentView(view);
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.service_layout);
+        setStatusBarLightMode(GetServiceActivity.this);
+        ButterKnife.bind(this);
+        initTopBar();
+    }
+
+    private void initTopBar(){
+        topBar.setTitle("获得的服务");
+        topBar.addLeftImageButton(R.mipmap.fanhui,R.id.fanhui).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
